4915c30c7242c3b1cd5c8a4482970aed807768b9
charles <cgiguere@iname.com>
2023-11-11 16:37:33 -0500

    neopixels

--- mp3_busy_pin.ino
+++ mp3_busy_pin.ino
@@ -1,12 +1,20 @@
 #include "SoftwareSerial.h"
 #include "DFRobotDFPlayerMini.h"
+#include <Adafruit_NeoPixel.h>
 
 #define RX_PIN 10
 #define TX_PIN 11
-#define BUSY_PIN 13
+#define BUSY_PIN 8
 
 DFRobotDFPlayerMini mp3;
 SoftwareSerial *softwareSerialMP3;
+
+// Neopixels
+#define PIXELSPIN 7
+#define PIXELSCOUNT 14
+
+Adafruit_NeoPixel pixels(PIXELSCOUNT, PIXELSPIN, NEO_GRB + NEO_KHZ800);
+#define COLOR_CYAN pixels.Color(0, 255, 255)
 
 void setup(void)
 {
@@ -14,8 +22,12 @@ void setup(void)
 
     Serial.begin(9600);
     initMP3();
+    initPixels();
     mp3.volume(25);
-    mp3.play(5);
+    mp3.play(7);
+    pixels.fill(COLOR_CYAN);
+    pixels.show();
+
     while (readBusyPin() == HIGH)
     {
         /* wait for play sound to start */
@@ -23,16 +35,40 @@ void setup(void)
     Serial.println("Playing sound...");
     while (readBusyPin() == LOW)
     {
-        digitalWrite(LED_BUILTIN,HIGH);
-        delay(200);
-        digitalWrite(LED_BUILTIN,LOW);
-        delay(200);
+        // digitalWrite(LED_BUILTIN, HIGH);
+        // delay(200);
+        // digitalWrite(LED_BUILTIN, LOW);
+        // delay(200);
+        showPixelsRandomLevel();
+        delay(60);
     }
+    pixels.clear();
+    pixels.show();
     Serial.println("Sound finished playing.");
 }
 
 void loop(void)
 {
+}
+
+void initPixels(void)
+{
+    pixels.begin();
+    pixels.setBrightness(100);
+    pixels.clear();
+    pixels.show();
+}
+
+void showPixelsRandomLevel(void)
+{
+    int level = random(PIXELSCOUNT);
+    pixels.clear();
+    pixels.show();
+    for (int i = 0; i < level; i++)
+    {
+        pixels.setPixelColor(i, COLOR_CYAN);
+    }
+    pixels.show();
 }
 
 int readBusyPin(void)
